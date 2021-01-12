22bb5593f61b00801343eb1356e4f236a0eaf2ea
Chanwoo Lee <leechanwoo25@gmail.com>
2021-01-12 15:08:02 +0900

    FinalVoc

--- runkitti.sh
+++ runkitti.sh
@@ -6,23 +6,23 @@
 
 
 # Kitti Sequence [00]
-#  ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI00-02.yaml ~/Dataset/dataset/sequences/00
+#  ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI00-02.yaml ~/Dataset/dataset/sequences/00
 
 
 # Kitti Sequence [04] 
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_demo
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_demo
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v5_b
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v5_b
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_c
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_c
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_d
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_d
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_e
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/04 04_v4_e
 
 
 # Kitti Sequence [05] => Kitti 5번 데모 실행 <=
-./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_Demo
+./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_Demo
 
 
 
@@ -40,10 +40,10 @@
 
 
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_b
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_b
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_c
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_c
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_d
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_d
 # sleep 10s
-# ./Examples/Monocular/sp_mono_kitti Voca/tempVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_e
+# ./Examples/Monocular/sp_mono_kitti Voca/FinalVoc.txt ./Examples/Monocular/SPKITTI04-12.yaml ~/Dataset/dataset/sequences/05 05_v6_e
--- runtum.sh
+++ runtum.sh
@@ -7,68 +7,68 @@
 #####################################
 
 # fr1_xyz 
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM_fr1_xyz.yaml /home/leecw/Dataset/TUM/fr1_xyz/ Demo_0
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM_fr1_xyz.yaml /home/leecw/Dataset/TUM/fr1_xyz/ Demo_0
 
 # fr2_xyz 
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM_fr2_xyz.yaml /home/leecw/Dataset/TUM/fr2_xyz/ fr2_xyz_demo
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM_fr2_xyz.yaml /home/leecw/Dataset/TUM/fr2_xyz/ fr2_xyz_demo
 
 # fr1_floor
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM1.yaml /home/leecw/Dataset/TUM/fr1_floor/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM1.yaml /home/leecw/Dataset/TUM/fr1_floor/
 
 # fr1_desk
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM1.yaml /home/leecw/Dataset/TUM/fr1_desk/ fr1_desk_demo
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM1.yaml /home/leecw/Dataset/TUM/fr1_desk/ fr1_desk_demo
 
 # fr2_360_kidnap
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_360_kidnap/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_360_kidnap/
 # Examples/Monocular/sp_mono_tum Voca/SPVoc1.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_360_kidnap/
 
 # fr2_desk # Good
-Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ a
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ b
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ c
+Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ a
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ b
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk/ c
 
 # fr3_long_office
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_long_office/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_long_office/
 
 #####################################
 ####             FR2             ####
 #####################################
 
 # fr3_nstr_tex_far
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_nstr_tex_far/ fr3_nstr_tex_far_demo
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_nstr_tex_far/ fr3_nstr_tex_far_demo
 
 # fr3_nstr_tex_near
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_nstr_tex_near/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_nstr_tex_near/
 
 # fr3_str_tex_far
-#  Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_demo
+#  Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_demo
 #  sleep 10s
-#  Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6b
+#  Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6b
 #  sleep 10s
-#  Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6c
+#  Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6c
 #  sleep 10s
-#  Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6d
+#  Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6d
 #  sleep 10s
-#  Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6e
+#  Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_far/ fr3_str_tex_far_6e
 
 # fr3_str_tex_near
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_near/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_str_tex_near/
 
 #####################################
 ####             FR3             ####
 #####################################
 
 # fr2_desk_person
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk_person/ demo
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM2.yaml /home/leecw/Dataset/TUM/fr2_desk_person/ demo
 
 # fr3_sit_xyz
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_sit_xyz/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_sit_xyz/
 
 # fr3_sit_halfsph
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_sit_halfsph/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_sit_halfsph/
 
 # fr3_walk_xyz
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_walk_xyz/ fr3_walk_xyz_demo
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_walk_xyz/ fr3_walk_xyz_demo
 
 # fr3_walk_halfsph
-# Examples/Monocular/sp_mono_tum Voca/tempVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_walk_halfsph/
+# Examples/Monocular/sp_mono_tum Voca/FinalVoc.txt Examples/Monocular/SPTUM3.yaml /home/leecw/Dataset/TUM/fr3_walk_halfsph/
